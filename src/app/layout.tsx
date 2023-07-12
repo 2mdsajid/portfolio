6aa385b7c666d5792f4a7d177ead06952382106f
2mdsajid <2mdsajid@gmail.com>
2023-07-12 15:17:23 +0545

    hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,5 @@
 import ThemeWrapper from '@/lib/utils/ThemeWrapper'
 import './globals.css'
-// import Hotjar from '@hotjar/browser';
-
-// const siteId = 3565377;
-// const hotjarVersion = 6;
-
-// Hotjar.init(siteId, hotjarVersion);
-
-import { Inter } from 'next/font/google'
-
 
 export const metadata = {
   title: 'Sajid',
